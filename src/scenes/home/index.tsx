314907226d7f9c391904e5c6fcc170bcb288332a
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-13 17:39:27 -0400

    completed benefits section

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -19,8 +19,9 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <motion.div className="md:flex mx-auto w-5/6 items-center justify-center h-5/6"
-      onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      <motion.div
+        className="md:flex mx-auto w-5/6 items-center justify-center h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
@@ -48,15 +49,16 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
           {/* ACTIONS */}
-          <motion.div className="mt-8 flex items-center gap-8"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
           >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
